c620b95b62ddf6ce9d2496e23a1798af19e807e4
hrkrshnn <webmail.hari@gmail.com>
2021-03-01 19:05:59 +0100

    Evaluate `keccak(a, const)` if value at memory location `a` is known
    Here the value of constant can be at most 32.

--- libyul/optimiser/LoadResolver.cpp
+++ libyul/optimiser/LoadResolver.cpp
@@ -23,13 +23,24 @@
 #include <libyul/optimiser/LoadResolver.h>
 
 #include <libyul/backends/evm/EVMDialect.h>
+#include <libyul/backends/evm/EVMMetrics.h>
 #include <libyul/optimiser/Semantics.h>
 #include <libyul/optimiser/CallGraphGenerator.h>
 #include <libyul/SideEffects.h>
 #include <libyul/AST.h>
+#include <libyul/Utilities.h>
+
+#include <libevmasm/Instruction.h>
+#include <libevmasm/GasMeter.h>
+#include <libsolutil/Keccak256.h>
+
+#include <range/v3/range/conversion.hpp>
+#include <range/v3/view/take.hpp>
 
 using namespace std;
 using namespace solidity;
+using namespace solidity::util;
+using namespace solidity::evmasm;
 using namespace solidity::yul;
 
 void LoadResolver::run(OptimiserStepContext& _context, Block& _ast)
@@ -38,7 +49,8 @@ void LoadResolver::run(OptimiserStepContext& _context, Block& _ast)
 	LoadResolver{
 		_context.dialect,
 		SideEffectsPropagator::sideEffects(_context.dialect, CallGraphGenerator::callGraph(_ast)),
-		!containsMSize
+		!containsMSize,
+		_context.expectedExecutionsPerDeployment
 	}(_ast);
 }
 
@@ -46,13 +58,30 @@ void LoadResolver::visit(Expression& _e)
 {
 	DataFlowAnalyzer::visit(_e);
 
+	auto isKeccak = [&](YulString const& _name) -> bool
+	{
+		if (auto evmDialect = dynamic_cast<EVMDialect const*>(&m_dialect))
+			if (auto builtin = evmDialect->builtin(_name))
+				return (
+					builtin->instruction &&
+					*builtin->instruction == Instruction::KECCAK256
+				);
+
+		return false;
+	};
+
 	if (FunctionCall const* funCall = std::get_if<FunctionCall>(&_e))
+	{
 		for (auto location: { StoreLoadLocation::Memory, StoreLoadLocation::Storage })
 			if (funCall->functionName.name == m_loadFunctionName[static_cast<unsigned>(location)])
 			{
 				tryResolve(_e, location, funCall->arguments);
 				break;
 			}
+
+		if (isKeccak(funCall->functionName.name))
+			tryEvaluateKeccak(_e, funCall->arguments);
+	}
 }
 
 void LoadResolver::tryResolve(
@@ -75,4 +104,62 @@ void LoadResolver::tryResolve(
 		if (auto value = util::valueOrNullptr(m_memory, key))
 			if (inScope(*value))
 				_e = Identifier{locationOf(_e), *value};
+}
+
+void LoadResolver::tryEvaluateKeccak(
+	Expression& _e,
+	std::vector<Expression> const& _arguments
+)
+{
+	GasMeter gasMeter{
+		dynamic_cast<EVMDialect const&>(m_dialect),
+		!m_expectedExecutionsPerDeployment,
+		m_expectedExecutionsPerDeployment ? *m_expectedExecutionsPerDeployment : 1
+	};
+
+	auto costOfKeccak = gasMeter.costs(_e);
+	auto costOfLiteral = gasMeter.costs(
+		Literal{
+			{},
+			LiteralKind::Number,
+			// a dummy 256-bit number to represent the Keccak256 hash.
+			YulString{u256(-1).str()},
+			{}
+		}
+	);
+
+	// We skip if there are no net gas savings.
+	// Note that for default `m_runs = 200`, the values are
+	// `costOfLiteral = 7200` and `costOfKeccak = 9000` for runtime context.
+	// For creation context: `costOfLiteral = 531` and `costOfKeccak = 90`.
+	if (costOfLiteral > costOfKeccak)
+		return;
+
+	yulAssert(_arguments.size() == 2, "");
+	Identifier const* memoryKey = std::get_if<Identifier>(&_arguments.at(0));
+	Identifier const* offset = std::get_if<Identifier>(&_arguments.at(1));
+
+	if (!memoryKey || !offset)
+		return;
+
+	if (
+		auto memoryValue = util::valueOrNullptr(m_memory, memoryKey->name);
+		memoryValue &&
+		inScope(*memoryValue)
+	)
+	{
+		optional<u256> memoryContent = valueOfIdentifier(*memoryValue);
+		optional<u256> memoryOffset = valueOfIdentifier(offset->name);
+		if (memoryContent && memoryOffset && *memoryOffset <= 32)
+		{
+			bytes contentAsBytes = toBigEndian(*memoryContent);
+			bytes slicedBytes = contentAsBytes | ranges::views::take(unsigned(*memoryOffset)) | ranges::to<vector>;
+			_e = Literal{
+				locationOf(_e),
+				LiteralKind::Number,
+				YulString{u256(keccak256(slicedBytes)).str()},
+				m_dialect.defaultType
+			};
+		}
+	}
 }
